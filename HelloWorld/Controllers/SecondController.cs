22f3f03edb3cc1a8ee38a2b157dec8114df41b68
Yurii Trofimenko <tyaa@ukr.net>
2018-07-15 19:17:03 +0300

    init

---
+++ HelloWorld/Controllers/SecondController.cs
@@ -0,0 +1,20 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            ViewBag.x = 12;
+            Person person = new Person() {id = 15, name = "Yurii" };
+            return View(person);
+        }
+    }
+}
